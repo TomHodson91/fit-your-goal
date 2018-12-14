7687bf093d0978de40f87a90c49efb456ea61832
Tom Hodson <tomhodson91@hotmail.com>
2018-12-14 14:45:33 +0000

    Updates

---
+++ precache-manifest.b2667ff93b2d93071216fb44fc88e85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d36c078481e79244f3c139f5c1c0c2f9",
+    "url": "/fit-your-goal/static/media/hero.d36c0784.png"
+  },
+  {
+    "revision": "2bd1c0db8f4f92db2626",
+    "url": "/fit-your-goal/static/js/runtime~main.2bd1c0db.js"
+  },
+  {
+    "revision": "36abaaca2de15862351f",
+    "url": "/fit-your-goal/static/js/main.36abaaca.chunk.js"
+  },
+  {
+    "revision": "268042be424f54edcb02",
+    "url": "/fit-your-goal/static/js/1.268042be.chunk.js"
+  },
+  {
+    "revision": "36abaaca2de15862351f",
+    "url": "/fit-your-goal/static/css/main.0ffa7cbe.chunk.css"
+  },
+  {
+    "revision": "b1c06e23c618dfefa95e585da1721caa",
+    "url": "/fit-your-goal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/precache-manifest.85d604520f8b4b5984657346442437cc.js"
+  "/fit-your-goal/precache-manifest.b2667ff93b2d93071216fb44fc88e85e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/index.html", {
+workbox.routing.registerNavigationRoute("/fit-your-goal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
