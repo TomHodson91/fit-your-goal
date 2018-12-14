8fe93096f5eb575370092e75ebfbfbbe51271b11
Tom Hodson <tomhodson91@hotmail.com>
2018-12-14 14:45:00 +0000

    Updates

---
+++ precache-manifest.85d604520f8b4b5984657346442437cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d36c078481e79244f3c139f5c1c0c2f9",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/static/media/hero.d36c0784.png"
+  },
+  {
+    "revision": "b9f12f899fd5f833642b",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/static/js/runtime~main.b9f12f89.js"
+  },
+  {
+    "revision": "9ac89728aadd2acd17a1",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/static/js/main.9ac89728.chunk.js"
+  },
+  {
+    "revision": "268042be424f54edcb02",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/static/js/1.268042be.chunk.js"
+  },
+  {
+    "revision": "9ac89728aadd2acd17a1",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/static/css/main.0ffa7cbe.chunk.css"
+  },
+  {
+    "revision": "dae0794125c0ff72d7c1a0680b8083d4",
+    "url": "%7BTomHodson91%7D.github.io/%7Bfit-your-goal%7D/index.html"
+  }
+];
