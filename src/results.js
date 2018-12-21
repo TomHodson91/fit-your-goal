449631155be250b969d29d0fe929a3d04bdfa8bf
Tom Hodson <tomhodson91@hotmail.com>
2018-12-21 14:15:36 +0000

    change

--- src/results.js
+++ src/results.js
@@ -10,7 +10,7 @@ export default function results(props) {
     return (
         <div className='results-info'>
             <h1>Your personal fitness program!</h1>
-            <h2>Try these activities: {getSports(Number(props.history.location.state.age), Number(props.history.location.state.weight),(props.history.location.state.goals)).join(', ')}</h2>
+            <h2>Try these activities: {getSports(Number(props.history.location.state.age), Number(props.history.location.state.weight),(props.history.location.state.goal)).join(', ')}</h2>
             <h2>Train between {getDuration(Number(props.history.location.state.age), Number(props.history.location.state.activity))[0]} and {getDuration(Number(props.history.location.state.age), Number(props.history.location.state.activity)).slice(-1)} minutes!</h2>
             <h2>Train up to {getFrequency(Number(props.history.location.state.age), Number(props.history.location.state.activity)).slice(-1)} time/s per week!</h2>
             <h2>Session intensity: {getIntensity(Number(props.history.location.state.age), Number(props.history.location.state.activity)).join(', ')}</h2>
